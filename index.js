fcdbd5e9284e7a6a24890a5537747a7ff4b94be0
M0un1R <mounirgaiby@gmail.com>
2022-03-06 16:20:00 +0100

    Half Way top sale carousel

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
-    
-    //banner for owl
-    $("#banner-area .owl-carousel").owlCarousel({
-        dots:true,
-        items:1
-    });
+  //banner for owl
+  $("#banner-area .owl-carousel").owlCarousel({
+    dots: true,
+    items: 1,
+  });
 
+  //top sale
+  $("#top-sale .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    responsiveClass: true,
+    responsive: { 0: { items: 1 }, 600: { items: 3 }, 1000: { items: 5 } },
+  });
 });
