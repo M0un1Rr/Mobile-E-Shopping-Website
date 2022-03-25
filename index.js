4c2bf9df22cc6f07e3538807dfd8abfb506c9894
M0un1R <mounirgaiby@gmail.com>
2022-03-25 19:27:44 +0100

    Special price complete removed extra css files

--- index.js
+++ index.js
@@ -16,4 +16,15 @@ $(document).ready(function () {
     responsiveClass: true,
     responsive: { 0: { items: 1 }, 600: { items: 3 }, 1000: { items: 5 } },
   });
+  //iso tope
+  var $grid = $(".grid").isotope({
+    itemSelector: ".grid-item",
+    layoutMode: "fitRows",
+  });
+
+  // filter items click
+  $(".button-group").on("click", "button", function () {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+  });
 });
